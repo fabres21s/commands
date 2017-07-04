6b6e220a129de6286a2898a2dc57c9d43b275ff6
Fabio Sierra <fabres21s@gmail.com>
2017-07-03 19:36:25 -0500

    war spring

--- linux/install.sh
+++ linux/install.sh
@@ -3,3 +3,6 @@ firewall-cmd --zone=public --add-port=8080/tcp --permanent
 
 yum -y install haproxy.x86_64
 
+
+### para habilitar haproxy en cualquier otro puerto
+setsebool -P haproxy_connect_any=1
